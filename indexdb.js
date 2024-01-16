39a3d27a7aa251ffc867454ce6f9bfaf91d7c975
kyagm <cloud_kumo_nuvola@yahoo.com>
2024-01-16 20:37:53 +0800

    Implemented table loading and reloading functions. Refined popup CSS styles.

--- indexdb.js
+++ indexdb.js
@@ -12,78 +12,148 @@ object = {
 }
 */
 
-// Initialize array to hold Project instances
-const projectList = [];
 
-// Project and Task classification
-class Project {
-
-    constructor(name, startDate, dbKey) {
-        this.name = name;
-        this.startDate = startDate;
-        this.tasks = [];
-        this.dbKey = dbKey;
-        this._lastFinished = null;
-    }
-
-
-    // Methods
-    addNewTask(task) {
-        this.tasks.push(task);
-        this.saveToDB();
-    }
-
-    deleteTask(index) {
-        if (index.isNumber() && index < this.tasks.length && index >= 0) {
-            return this.tasks.splice(index, 1);
-        }
-        this.saveToDB();
-    }
-    
-    finishCurrentTask() {
-        if (this.tasks.length > 0) {
-            this._lastFinished = this.tasks.shift();
-        }
-        this.saveToDB();
-    }
-
-    saveToDB() {
-        // Save current object properties into db
-    }
-
-    // Getters    
-    get currentTask() {
-        return this.tasks[0];
-    }
-    get lastFinished() {
-        return this._lastFinished;
-    }
-}
-
+// Query selectors
 const projectName = document.querySelector("#projectName");
 const addButton = document.querySelector("#addButton");
 const display = document.querySelector("#display");
 
-addButton.addEventListener("click", (e) => {
-    e.preventDefault();
 
-    const project = document.createElement("div");
-    const date = document.createElement("span")
-    const today = new Date();
-    date.textContent = today.getMonth() + "/" + today.getDate() + "/" + today.getFullYear();
+// IndexedDB tutorial
+let db;
+const request = window.indexedDB.open("MyTestDatabase", 3); /* Open the db
+this returns an IDBOpenDBRequest object with result and error properties
+the 2nd parameter is a number indicating the version which determines the db schema.
+if the db doesn't exit, open creates it and triggers an onupgradeneeded event.
+if it exists but with a higher version, onupgradeneeded is also triggered. */
 
-    const object = new Project(projectName.value, date.textContent, 0);
-    projectList.push(object);
+// Setting up event handlers on onerror() and onsuccess() functions 
+request.onerror = (e) => {
+    console.error("Permission denied by user.")
+};
+
+request.onsuccess = (e) => {
+    db = e.target.result;
+};
+/* If everything goes wll, a success event (a DOM event whose type property is set to "success") is fired with request as its target.
+request.onsuccess is triggered with the success event as the argument.
+Otherwise, error is fired instead of success.
+    Common causes of error when opening a db:
+    > user does not give permission to app to create a db
+*/
+
+db.onerror = (e) => {
+    console.error(`DB error: $(event.target.errorCode)`);
+};
+/* Much like onclick, events like onerror also bubble up to the parent.
+In this case, the parent of the event's target (request) would be the transaction whose parent would then be the db.
+So if you attach an onerror handler to the db, it will also trigger if any error happens within its descendants.
+*/
+
+// I love it when things make sense
+
+request.onupgradeneeded = (e) => {
+    const db = e.target.result; // save the resulting db that was returned by the request
+    const objectStore = db.createObjectStore('name', {keyPath: 'myKey'});
+    /* You do not need to create object stores that are already in the older versions, but you can delete them if needed
+    To update, you need to delete and recreate
+    'myKey' in this case is one of the properties of the data object that is presumably unique
+    */
+    const objStore = db.createObjectStore('names', {autoIncrement: true });
+    /* this ^ is a key generator which automatically starts from 1 and increments by 1
+    It can only be accessed in the success event when adding the data the key is assigned to.
+    You need to make sure to catch the result value of the target like so:
     
-    const deleteButton = document.createElement("button");
-    deleteButton.textContent = 'Delete';
-    deleteButton.addEventListener('click', () => {
-        display.removeChild(project);
-        projectList.pop(project);
-    })
+    const keyValue = e.target.result
 
-    project.textContent = object.name;
-    project.appendChild(date);
-    project.appendChild(deleteButton);
-    display.appendChild(project);
-})
+    where e is the event, target is the add request, and result is the key value
+    */
+};
+/* This event is triggered when you create a new database or open an existing db with a higher version number.
+you create the object stores needed for the new version of the db in the event handler.
+This event is implimented only in recent browsers.
+
+Creating object stores with the same name or deleting non-existing object stores will give an error
+
+after succesfully going through the onupgradeneeded event, the onsuccess will then proceed
+*/
+
+const transaction = db.transaction(["customers"], "readwrite");
+/* argument 1 is a list of object stores that you want
+argument 2 is the mode. other option is readonly (and versionchange)
+*/
+
+transaction.oncomplete = (e) => {
+    console.log("All done!");
+};
+
+transaction.onerror = (e) => {
+    // Add error handlers here
+}
+
+const objectStore = transaction.objectStore("customers");
+customerData.forEach((customer) => {
+    const request = objectStore.add(customer);
+    request.onsuccess = (e) => {
+        // e.target.result === customer.ssn;
+    };
+});
+
+// You can also compress the transaction -> request. Since the error bubbles up to db, request.onsuccess won't ever happen if an error is triggered in any of the steps
+const request = db
+    .transaction(["customers"], "readwrite")
+    .objectStore("customers")
+    .delete("444-44-4444");
+request.onsuccess = (e) => {
+    // Deleted!
+}
+
+const transaction = db.transaction(["customers"]); // defaults to readonly
+const objectStore = transaction.objectStore("customers");
+const request = objectStore.get("444-44-4444");
+request.onerror = (e) => {
+    // handle errors here
+};
+request.onsuccess = (e) => {
+    // do smth with request.result
+};
+
+// even shorter ver as long as you have db error handler
+db
+    .transaction("customers", "readwrite") // can skip the list since it's just 1 obj store
+    .objectStore("customers")
+    .get("444-44-4444").onsuccess = (e) => {
+        // the event is a success attached to the get request with the result of the object data
+        const data = e.target.result;
+
+        data.age = 42;
+
+        const requestUpdate = objectStore.put(data);
+        requestUpdate.onerror = (e) => {
+            // error blah
+        };
+        requestUpdate.onsuccess = (e) => {
+            // data is updated inside the db;
+        };
+    };
+
+// if you want to iterate through the whole object store
+const customers = [];
+
+objectStore.openCursor().onsuccess = (e) => {
+    const cursor = e.target.result;
+    if (cursor) {
+        customers.push(cursor.value);
+        cursor.continue;
+    }
+    else {
+        console.log("No more entries!");
+    }
+};
+
+// if you just want everything in a an array
+objectStore.getAll().onsuccess = (e) => {
+    customers = e.target.result; // the result is an array of objects (like a list of dictionaries)
+}
+
+console.log(`${objectStore} is the object store`)
--- main.js
+++ main.js
@@ -1,3 +1,218 @@
+// Back-end objects
+let projects = [];
+let highestKey = 0; // TODO: onload, update highestKey based on the last item on project list
+let openedSidePopup; // tracks the current open side popup. Only 1 can be open at a time
+
+class Project {
+    constructor(name, taskList, startDate) {
+        this.name = name;
+        this.tasks = taskList;
+        this.startDate = startDate;
+        this.key = highestKey + 1;
+        
+        this.lastFinished = null; // When loading from db, assign this directly after the constructor
+    }
+}
+
+function finishTask(e) {
+    const projectName = e.currentTarget.parentElement.parentElement.querySelector('.projectName').textContent;
+    const currentRow = e.currentTarget.closest('tr');
+
+    const project = projects.filter((object) => {
+        return object.name === projectName;
+    })
+
+    if (project[0].tasks.length > 0) {
+        project[0].lastFinished = project[0].tasks.shift();
+        updateRow(currentRow, project[0]);
+        console.log(`"${project[0].lastFinished}" moved to Last Finished.`);
+    }
+    else {
+        console.log(`No task available.`)
+    }
+    
+}
+
+// Query selectors
+const newProjectWindow = document.querySelector("#newProjectPopup");
+const newProjectButton = document.querySelector("#newProjectButton");
+const overlay = document.querySelector("#overlay");
+const submitNewProject = document.querySelector("#newProjectPopup button")
+const closeButtons = document.querySelectorAll(".close");
+const projectName = document.querySelector("#projectName");
+const projectTask = document.querySelector("#projectTask");
+const dateStarted = document.querySelector("#dateStarted");
+const tbody = document.querySelector('tbody');
+
+// General pop up functions
+for (const button of closeButtons) {
+    button.addEventListener('click', closePopup);
+}
+
+function closePopup(e) {
+    const popup = e.target.closest(".popup");
+
+    const isHidden = getComputedStyle(popup).display === 'none';
+    const overlaid = getComputedStyle(popup).display === 'block';
+
+    if (!isHidden) {
+        popup.style.display = 'none';
+    }
+    if (overlaid) {
+        overlay.style.display = 'none';
+    }
+}
+
+function openTaskPopup(e) {
+    // currenTarget is td element with children [span, div]
+    const popup = e.currentTarget.querySelector('.popup');
+
+    if (e.target !== e.currentTarget && e.target !== e.currentTarget.querySelector('span')) { // currentTarget is the element where the listener is attached to. target is the element that triggered the event
+        return;
+    }
+
+    const isHidden = getComputedStyle(popup).display === 'none';
+
+    if (isHidden) {
+        popup.style.display = 'block';
+        if (openedSidePopup && openedSidePopup !== popup) {
+            openedSidePopup.style.display = 'none';
+        }
+        openedSidePopup = popup;
+    }
+    else {
+        popup.style.display = 'none';
+    }
+}
+
+// New Project
+newProjectButton.addEventListener('click', openNewProjectWindow);
+submitNewProject.addEventListener('click', newProject);
+
+function openNewProjectWindow() {
+    const isHidden = getComputedStyle(newProjectWindow).display === 'none';
+
+    if (isHidden) {
+        newProjectWindow.style.display = 'block';
+        overlay.style.display = 'block';
+    }
+    else {
+        newProjectWindow.style.display = 'none';
+        overlay.style.display = 'none';
+    }
+}
+
+function newProject(e) {
+    e.preventDefault()
+    
+    // Validate form inputs
+    if (projectName.value && projectTask.value && dateStarted.value) {
+        const project = new Project(projectName.value, [projectTask.value], dateStarted.value);
+        console.log(`Created new project: ${projectName.value}`);
+        highestKey++;
+        console.log(`Next key: ${highestKey}`)
+        projects.push(project);
+
+        // close popup window and overlay
+        newProjectWindow.style.display = 'none';
+        overlay.style.display = 'none';
+        projectName.value = null;
+        projectTask.value = null;
+        dateStarted.value = null;
+
+        // Reload table to reflect new project
+        createNewRow(project);
+    }
+    else {
+        alert("Make sure all input fields have a value.")
+    }
+}
+
+// Table loading functions
+
+function reloadTable() {
+    clearTable();
+    for (const project of projects) {
+        createNewRow(project);
+    }
+    console.log(`Table reloaded`);
+}
+
+function updateRow(row, project) {
+    row.querySelector('.projectName').textContent = project.name;
+    row.querySelector('.lastFinished').textContent = project.lastFinished;
+    if (project.tasks[0]) {
+        row.querySelector('.current span').textContent = project.tasks[0];    
+    }
+    else {
+        row.querySelector('.current span').textContent = 'Add task...';
+    }
+    
+    console.log('Popup update function not available yet.')
+
+}
+
+function createNewRow(project) {
+    const row = document.createElement('tr');
+    const name = document.createElement('td');
+    const lastFinished = document.createElement('td');
+    const currentTask = document.createElement('td');
+        // currentTask children
+        const taskName = document.createElement('span');
+        const sidePopup = document.createElement('div');
+            // Popup
+            const header = document.createElement('div');
+            const title = document.createElement('h3');
+            const closeButton = document.createElement('img');
+    const finishButton = document.createElement('td');
+        const icon = document.createElement('img');
+
+    name.textContent = project.name;
+    name.setAttribute('class', 'projectName');
+    lastFinished.textContent = project.lastFinished;
+    lastFinished.setAttribute('class', 'lastFinished');
+    currentTask.setAttribute('class', 'current');
+    currentTask.addEventListener('click', openTaskPopup);
+    
+        taskName.textContent = project.tasks[0];
+        taskName.setAttribute('class', 'button');
+
+        // popup construction
+        sidePopup.setAttribute('class', 'popup side');
+            header.setAttribute('class', 'header');
+            title.textContent = `${project.name}: Tasks`;
+            closeButton.setAttribute('class', 'icon close');
+            closeButton.setAttribute('src', 'images/close-svgrepo-com.svg');
+            closeButton.addEventListener('click', closePopup);
+            header.appendChild(title);
+            header.appendChild(closeButton);
+        sidePopup.appendChild(header);
+    
+    icon.setAttribute('class', 'icon check');
+    icon.setAttribute('src', 'images/check.svg');
+    icon.addEventListener('click', finishTask);
+    finishButton.appendChild(icon);
+    finishButton.setAttribute('class', 'finishTaskButtons')
+
+    currentTask.appendChild(taskName);
+    currentTask.appendChild(sidePopup);
+    row.appendChild(name);
+    row.appendChild(lastFinished);
+    row.appendChild(currentTask);
+    row.appendChild(finishButton);
+    tbody.appendChild(row);
+
+    console.log(`Project ${project.name} added to table`);
+}
+
+function clearTable() {
+    while (tbody.firstChild) {
+        tbody.removeChild(tbody.firstChild);
+    }
+    console.log('Removed all table contents.')
+}
+
+/*
 // Pop-up divs testing
 const taskQueue = document.querySelectorAll(".current-task");
 const closeButton = document.querySelectorAll(".close");
@@ -127,7 +342,7 @@ When clicking the (+) button or loading project objects from db,
 use createNewRow() function to insert into table.
 
 This function accepts a Project object.
-*/
+
 function createNewRow(project) {
     const table = document.querySelector('table');
     const row = document.createElement('tr');
@@ -166,4 +381,4 @@ function createNewRow(project) {
 
     table.appendChild(row);
 }
-
+*/
