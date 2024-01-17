43ac61501ffd0a025a468a61d6be235e91514ca2
kyagm <cloud_kumo_nuvola@yahoo.com>
2024-01-17 19:53:39 +0800

    Implemented task addition functions and related popups. Current version needs refactoring in main JS file. My head hurts when I look at it.

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
+// Query selectors
+const newProjectWindow = document.querySelector("#newProjectPopup");
+const newTaskWindow = document.querySelector("#newTaskPopup");
+const newProjectButton = document.querySelector("#newProjectButton");
+const overlay = document.querySelector("#overlay");
+const submitNewProject = document.querySelector("#newProjectPopup button");
+const addNewTask = document.querySelector("#newTaskPopup button");
+const closeButtons = document.querySelectorAll(".close");
+const projectName = document.querySelector("#projectName");
+const projectTask = document.querySelector("#projectTask");
+const dateStarted = document.querySelector("#dateStarted");
+const newTaskField = document.querySelector("#newTask");
+const tbody = document.querySelector('tbody');
+
 // Back-end objects
 let projects = [];
 let highestKey = 0; // TODO: onload, update highestKey based on the last item on project list
 let openedSidePopup; // tracks the current open side popup. Only 1 can be open at a time
+let newTaskTarget; // tracks which project is currently being accessed by the task addition functions
 
 class Project {
     constructor(name, taskList, startDate) {
@@ -15,8 +30,8 @@ class Project {
 }
 
 function finishTask(e) {
-    const projectName = e.currentTarget.parentElement.parentElement.querySelector('.projectName').textContent;
     const currentRow = e.currentTarget.closest('tr');
+    const projectName = currentRow.querySelector('.projectName').textContent;
 
     const project = projects.filter((object) => {
         return object.name === projectName;
@@ -33,21 +48,22 @@ function finishTask(e) {
     
 }
 
-// Query selectors
-const newProjectWindow = document.querySelector("#newProjectPopup");
-const newProjectButton = document.querySelector("#newProjectButton");
-const overlay = document.querySelector("#overlay");
-const submitNewProject = document.querySelector("#newProjectPopup button")
-const closeButtons = document.querySelectorAll(".close");
-const projectName = document.querySelector("#projectName");
-const projectTask = document.querySelector("#projectTask");
-const dateStarted = document.querySelector("#dateStarted");
-const tbody = document.querySelector('tbody');
+addNewTask.addEventListener('click', () => {
+    const currentRow = document.querySelector('.projectName#' + newTaskTarget.name.replace(" ", "")).closest('tr');
+    
+    newTaskField.focus();
+    if (newTaskField.value) {
+        newTaskTarget.tasks.push(newTaskField.value);
+        updateRow(currentRow, newTaskTarget);
+        newTaskField.value = null;
+    }  
+})
 
 // General pop up functions
 for (const button of closeButtons) {
     button.addEventListener('click', closePopup);
 }
+
 
 function closePopup(e) {
     const popup = e.target.closest(".popup");
@@ -85,22 +101,36 @@ function openTaskPopup(e) {
     }
 }
 
-// New Project
-newProjectButton.addEventListener('click', openNewProjectWindow);
+function listTask(project) {
+    const ol = document.createElement('ol');
+    for (const task of project.tasks) {
+        const li = document.createElement('li');
+        li.textContent = task;
+        ol.appendChild(li);
+    }
+
+    return ol;
+}
+
+// New Project or Task
+newProjectButton.addEventListener('click', () => {
+    openWindowPopup(newProjectWindow);
+});
 submitNewProject.addEventListener('click', newProject);
 
-function openNewProjectWindow() {
-    const isHidden = getComputedStyle(newProjectWindow).display === 'none';
+function openWindowPopup(selector) {
+    const isHidden = getComputedStyle(selector).display === 'none';
 
     if (isHidden) {
-        newProjectWindow.style.display = 'block';
+        selector.style.display = 'block';
         overlay.style.display = 'block';
     }
     else {
-        newProjectWindow.style.display = 'none';
+        selector.style.display = 'none';
         overlay.style.display = 'none';
     }
 }
+
 
 function newProject(e) {
     e.preventDefault()
@@ -147,9 +177,10 @@ function updateRow(row, project) {
     else {
         row.querySelector('.current span').textContent = 'Add task...';
     }
-    
-    console.log('Popup update function not available yet.')
-
+    let ol = row.querySelector('ol');
+    row.querySelector('.popup').removeChild(ol);
+    ol = listTask(project);
+    row.querySelector('.popup').insertBefore(ol, row.querySelector('.popup').children[1]);
 }
 
 function createNewRow(project) {
@@ -164,11 +195,14 @@ function createNewRow(project) {
             const header = document.createElement('div');
             const title = document.createElement('h3');
             const closeButton = document.createElement('img');
+            const footer = document.createElement('div');
+            const addTaskButton = document.createElement('button');
     const finishButton = document.createElement('td');
         const icon = document.createElement('img');
 
     name.textContent = project.name;
     name.setAttribute('class', 'projectName');
+    name.setAttribute('id', project.name.replace(" ", ""));
     lastFinished.textContent = project.lastFinished;
     lastFinished.setAttribute('class', 'lastFinished');
     currentTask.setAttribute('class', 'current');
@@ -186,7 +220,22 @@ function createNewRow(project) {
             closeButton.addEventListener('click', closePopup);
             header.appendChild(title);
             header.appendChild(closeButton);
+
+            const ol = listTask(project);
+
+            footer.setAttribute('class', 'footer');
+            addTaskButton.textContent = '+';
+            addTaskButton.setAttribute('class', 'newTaskButton');
+            addTaskButton.addEventListener('click', () => {
+                newTaskField.value = null;
+                openWindowPopup(newTaskWindow);
+                newTaskTarget = project;
+                newTaskField.focus();
+            });
+            footer.appendChild(addTaskButton);
         sidePopup.appendChild(header);
+        sidePopup.appendChild(ol);
+        sidePopup.appendChild(footer);
     
     icon.setAttribute('class', 'icon check');
     icon.setAttribute('src', 'images/check.svg');
@@ -211,174 +260,3 @@ function clearTable() {
     }
     console.log('Removed all table contents.')
 }
-
-/*
-// Pop-up divs testing
-const taskQueue = document.querySelectorAll(".current-task");
-const closeButton = document.querySelectorAll(".close");
-const overlay = document.querySelector('#overlay');
-let openDiv = null;
-
-for (const project of taskQueue) {
-    project.addEventListener('click', popTaskDiv);
-}
-for (const button of closeButton) {
-    button.addEventListener('click', closePop);
-}
-
-
-function popTaskDiv(event) {
-    const popup = event.currentTarget.querySelector('div.popup.tasks');
-
-    if (event.target !== event.currentTarget) {
-        return;
-    }
-
-    isHidden = getComputedStyle(popup).display === 'none';
-
-    if (isHidden) {
-        popup.style.display = 'block';
-        if (openDiv && openDiv !== popup) { // AKA if there is an open div that is not our current target, close that
-            openDiv.style.display = 'none';
-        }
-    }
-    else {
-        popup.style.display = 'none';
-    }
-
-    openDiv = popup;
-}
-
-// New project popup window thingy
-
-const newButton = document.querySelector('#new-project');
-const closeNewProject = document.querySelector('#closeNewProject');
-
-newButton.addEventListener('click', newProject);
-closeNewProject.addEventListener('click', closePop);
-
-function newProject() {
-    const popupWindow = document.querySelector('.popup.window');
-
-    isHidden = getComputedStyle(popupWindow).display === 'none';
-
-    if (isHidden) {
-        popupWindow.style.display = 'block';
-        overlay.style.display = 'block';
-    }
-    else {
-        popupWindow.style.display = 'none';
-        overlay.style.display = 'none';
-    }
-    
-}
-
-function closePop(event) {
-    const popup = event.currentTarget.closest('.popup');
-    
-    const isOpen = getComputedStyle(popup).display === 'block';
-    const isOverlay = getComputedStyle(overlay).display === 'block';
-
-    if (isOpen) {
-        popup.style.display = 'none';
-    }
-    if (isOverlay) {
-        overlay.style.display = 'none';
-    }
-}
-
-
-
-
-// Project and Task classification
-class Project {
-
-    constructor(name, startDate) {
-        this.name = name;
-        this.startDate = startDate;
-        this.tasks = [];
-        this.dbKey = dbKey;
-        this._lastFinished = null;
-    }
-
-
-    // Methods
-    // Might need to set the functions to async once IndexedDB is used
-    addNewTask(task) {
-        this.tasks.push(task);
-        this.saveToDB();
-    }
-
-    deleteTask(index) {
-        if (index.isNumber() && index < this.tasks.length && index >= 0) {
-            return this.tasks.splice(index, 1);
-        }
-        this.saveToDB();
-    }
-    
-    finishCurrentTask() {
-        if (this.tasks.length > 0) {
-            this._lastFinished = this.tasks.shift();
-        }
-        this.saveToDB();
-    }
-
-    saveToDB() {
-        // Save current object properties into db
-    }
-
-    // Getters    
-    get currentTask() {
-        return this.tasks[0];
-    }
-    get lastFinished() {
-        return this._lastFinished;
-    }
-}
-
-
-/*
-When clicking the (+) button or loading project objects from db, 
-use createNewRow() function to insert into table.
-
-This function accepts a Project object.
-
-function createNewRow(project) {
-    const table = document.querySelector('table');
-    const row = document.createElement('tr');
-
-    const projectName = document.createElement('td');
-    const lastFinished = document.createElement('td');
-    const currentTask = document.createElement('td');
-    const checkIn = document.createElement('td');
-
-    projectName.textContent = project.name;
-    lastFinished.textContent = project.lastFinished;
-
-    const currentName = document.createElement('span');
-    currentName.setAttribute('class', 'current-task');
-    currentName.textContent = project.currentTask;
-    
-    const popup = document.createElement('div');
-    popup.setAttribute('class', 'popup tasks');
-    popup.textContent = 'Input field and display under construction';
-    currentName.appendChild(popup);
-    
-    const finishButton = document.createElement('button');
-    finishButton.setAttribute('class', 'finish-task table');
-    finishButton.addEventListener('click', project.finishCurrentTask); // Might need to use await once the db functions are set up
-    
-    currentTask.appendChild(currentName);
-    currentTask.appendChild(finishButton);
-    
-    const comments = document.createElement('input');
-    const checkInButton = document.createElement('button');
-    comments.setAttribute('placeholder', "Today's progress is...");
-    checkInButton.setAttribute('class', 'check-in table');
-    checkInButton.addEventListener('click', project.checkin); // Might need to use await once the db functions are set up
-    checkIn.appendChild(comments);
-    checkIn.appendChild(checkInButton);
-
-    table.appendChild(row);
-}
-*/
